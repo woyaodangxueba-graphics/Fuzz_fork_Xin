6425409696d7734eb900e681c6b7c7ef2bb2a3b3
Xin Li <urumican@126.com>
2014-12-02 20:42:19 -0800

    Change the header for generator.

--- generator.h
+++ generator.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
+
 // struct node
 // {
 // 	int val;
@@ -20,13 +20,12 @@
 struct para_pool
 {
 	int *fd_pool;
-	int *mode;
 	char *dirs_pool[1000];
-	// int *mode_pool[1000];
+	mode_t mode_pool[1000];
 };
 
 void generator_init(char *direct, int *files_number);
 void generator_fd_dir(struct para_pool *pool, char *direct, int *index);
 //void generator_dir(struct para_pool *pool, char *direct, int *index);
 void generator_init_fd(char *direct, int *index);
-void generator_mod();
+void generator_mod(struct para_pool *pool);
